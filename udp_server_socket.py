9f0c084f7c8de51e57d7166053f8e6e73180e8a8
vincentkao <vincentman0403@gmail.com>
2018-09-30 19:07:34 +0800

    use logging for udp_server_socket

--- udp_server_socket.py
+++ udp_server_socket.py
@@ -3,17 +3,33 @@ import sys
 from common import *
 import pickle
 import zlib
+import logging
 
 
 if __name__ == '__main__':
+    log_type = 'udp_server'
+    logger = logging.getLogger(log_type)
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(name)-12s %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+
+    log_file_name = '%s_%s.log' % (log_type, time.strftime("%Y-%m-%d", time.localtime()))
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M',
+                        handlers=[logging.FileHandler(log_file_name, 'a', 'utf-8'), console])
+
+
     config = load_config()
     print('config =>', config)
 
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        write_log('server', 'UDP socket created successfully...')
+        logger.debug('UDP socket created successfully...')
     except OSError as os_err:
-        write_log('server', '[ERROR]Create socket, OSError: %s' % os_err)
+        logger.error('Create socket, OSError: %s' % os_err)
         sys.exit(1)
 
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # reuse socket
@@ -25,16 +41,16 @@ if __name__ == '__main__':
             if not data:
                 print('No data received...')
             else:
-                write_log('server', 'Received %d bytes data from %s:%d' % (len(data), host, port))
+                logger.info('Received %d bytes data from %s:%d' % (len(data), host, port))
                 data = zlib.decompress(data)
                 detections = pickle.loads(data)
-                write_log('server', 'Data...%s' % detections, False)
+                logger.debug('Data...%s' % detections)
         except KeyboardInterrupt:
-            write_log('server', '[ERROR]KeyboardInterrupt')
+            logger.info('KeyboardInterrupt~~~')
             break
         except Exception as e:
-            write_log('server', '[ERROR]Exception: %s' % e)
+            logger.info('Exception: %s' % e)
             break
 
-    write_log('server', 'Close socket~~~')
+    logger.info('Close socket~~~')
     sock.close()
